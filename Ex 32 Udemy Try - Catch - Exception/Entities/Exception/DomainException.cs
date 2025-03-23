6b217aaaa4d0c8eb700dde3e68886d655ff2b8b0
Janerio Gustavo <janerio32@hotmail.com>
2025-03-23 20:55:54 -0300

    Tratamento de exception

---
+++ Ex 32 Udemy Try - Catch - Exception/Entities/Exception/DomainException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_32_Udemy_Try___Catch___Exception.Entities.Exception
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message) { }
+    }
+}
--- Ex 32 Udemy Try - Catch - Exception/Program.cs
+++ Ex 32 Udemy Try - Catch - Exception/Program.cs
@@ -1,1 +1,61 @@
-﻿
+﻿using Ex_32_Udemy_Try___Catch___Exception.Entities;
+using Ex_32_Udemy_Try___Catch___Exception.Entities.Exception;
+using System.Globalization;
+
+try
+{
+    while (true)
+    {
+        Console.WriteLine("Room number");
+        int rn = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Check-in date (dd/MM/yyyy): ");
+        DateTime checkin;
+        while (!DateTime.TryParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out checkin))
+        {
+            Console.WriteLine("Invalid format! Please enter the check-in date in the format dd/MM/yyyy:");
+        }
+
+        Console.WriteLine("Check-out date (dd/MM/yyyy): ");
+        DateTime checkout;
+        while (!DateTime.TryParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out checkout))
+        {
+            Console.WriteLine("Invalid format! Please enter the check-out date in the format dd/MM/yyyy:");
+        }
+
+        Reservation reservation = new Reservation(rn, checkin, checkout);
+        Console.WriteLine($"reservation:  {reservation}");
+
+        Console.WriteLine("Do you like to att the information (y/n): ");
+        string response = Console.ReadLine();
+
+        if (response == "y")
+        {
+            Console.WriteLine();
+            Console.WriteLine("Enter data to update the reservation: ");
+            Console.WriteLine("Check-in date (dd/MM/yyyy): ");
+            while (!DateTime.TryParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out checkin))
+            {
+                Console.WriteLine("Invalid format! Please enter the check-in date in the format dd/MM/yyyy:");
+            }
+
+            Console.WriteLine("Check-out date (dd/MM/yyyy): ");
+            while (!DateTime.TryParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out checkout))
+            {
+                Console.WriteLine("Invalid format! Please enter the check-out date in the format dd/MM/yyyy:");
+            }
+
+            reservation.UpdateDates(checkin, checkout);
+            Console.WriteLine($"reservation:  {reservation}");
+            break;
+        }
+        else
+        {
+            break;
+        }
+    }
+}catch(DomainException e)
+{
+    Console.WriteLine($"Error in reservation: {e.Message}");
+}
+
